67239794ebd86fe6c436975763aca9eb14f037bf
Danilo Augusto <danilo.augmonteiro@gmail.com>
2021-02-17 00:14:19 -0300

    Capa

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Box from '../src/components/foundation/Box';
 import Menu from '../src/components/commons/Menu';
 import Cover from '../src/components/commons/Cover';
+import Grid from '../src/components/foundation/Grid';
+import Text from '../src/components/foundation/Text';
+import Avatar from '../src/components/commons/Avatar';
 
 export default function Home() {
   return (
@@ -15,6 +18,52 @@ export default function Home() {
     >
       <Cover>
         <Menu />
+
+        <Grid.Container>
+          <Grid.Row paddingTop={30} paddingBottom={50}>
+            <Grid.Col value={{ xs: 12, md: 6 }}>
+              <Avatar
+                alt="Imagem de dois celulares com telas da aplicação."
+                src="img/avatar.jpg"
+              />
+            </Grid.Col>
+            <Grid.Col
+              offset={{ xs: 0, md: 0 }}
+              value={{ xs: 12, md: 5 }}
+              display={{ xs: 'block', md: 'flex' }}
+              alignItems="flex-start"
+              justifyContent="center"
+              flexDirection="column"
+              marginTop={{ xs: 30 }}
+            >
+              <Text
+                variant="title"
+                tag="h1"
+                display="inline"
+                color="white"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Danilo Augusto
+              </Text>
+              <Text
+                variant="paragraph1"
+                tag="p"
+                color="white"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industrys standard dummy text
+                ever since the 1500s.
+              </Text>
+            </Grid.Col>
+          </Grid.Row>
+        </Grid.Container>
       </Cover>
     </Box>
   );
---
+++ src/components/commons/Avatar/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Avatar = styled.img`
+  display: block;
+  margin: auto;
+  border-radius: 50%;
+  height: 250px;
+`;
+
+export default Avatar;
